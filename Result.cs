c2814a3563cf58bbc5bd5a13c2bb9ab3758da9d9
luowenyi <71054959+luowenyi@users.noreply.github.com>
2020-09-30 16:25:55 +0800

    Add files via upload

---
+++ Result.cs
@@ -0,0 +1,31 @@
+﻿using Renci.SshNet.Messages;
+using Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class Result<T>
+    {
+        public int Code = 1;
+        public string msg = "";
+        public T res = default(T);
+        public static Result Ok(T result) 
+        {
+            return new Result() { res = result };
+        }
+        public static Result Ok(string messago, T Result = default(T)) 
+        {
+            return new Result() { msg = messago, res = Result };
+        }
+        public static Result Err(string messago)
+        {
+            return new Result() { Code = -1, msg = messago };
+        }
+    }
+}
+public class Result : Result<dynamic>
+{
+}
